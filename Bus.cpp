1cadfb8e5ccd8d6f699e517ff6bc81ef608fb7f8
MikeHE-CODER <a1918791@adelaide.edu.au>
2024-08-27 13:02:27 +0930

    Update Bus.cpp

--- Bus.cpp
+++ Bus.cpp
@@ -2,10 +2,3 @@
 #include <ctime>
 #include <iostream>
 using namespace std;
-  Bus::Bus(){};
-  Bus::Bus(int ID) : Vehicle(ID) {}
-  Bus::Bus(time_t t, int ID) : Vehicle(t, ID) {}
-  int Bus::getParkingDuration(){
-    return 0.75 * (time(0) - this->gettimeOfEntry());
-  }
-
