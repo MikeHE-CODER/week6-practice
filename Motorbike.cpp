8669848e1bccd4ce01837e80b208fd158c439f64
MikeHE-CODER <a1918791@adelaide.edu.au>
2024-08-27 13:03:01 +0930

    Update Motorbike.cpp

--- Motorbike.cpp
+++ Motorbike.cpp
@@ -2,10 +2,4 @@
 #include <ctime>
 #include <iostream>
 using namespace std;
-Motorbike::Motorbike(){};
-Motorbike::Motorbike(int ID) : Vehicle(ID) {}
-Motorbike::Motorbike(time_t t, int ID) : Vehicle(t, ID) {}
-int Motorbike::getParkingDuration() override {
-    return 0.85 * (time(0) - this->gettimeOfEntry());
-  }
 
