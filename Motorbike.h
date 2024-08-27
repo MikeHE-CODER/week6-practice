9e888e6774ebfa52bef0c0160c86d39f4a005557
MikeHE-CODER <a1918791@adelaide.edu.au>
2024-08-27 11:46:09 +0930

    Create Motorbike.h

---
+++ Motorbike.h
@@ -0,0 +1,17 @@
+#ifndef motorbike_h
+#define motorbike_h
+#include "Vehicle.h"
+#include <ctime>
+#include <iostream>
+using namespace std;
+class Motorbike : public Vehicle {
+public:
+  Motorbike(){};
+  Motorbike(int ID) : Vehicle(ID) {}
+  Motorbike(time_t t, int ID) : Vehicle(t, ID) {}
+  int getParkingDuration() override {
+    return 0.85 * (time(0) - this->gettimeOfEntry());
+  }
+};
+
+#endif
