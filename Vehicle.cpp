39891a0114164ac93a5f20c84b7814f47d54571e
MikeHE-CODER <a1918791@adelaide.edu.au>
2024-08-27 11:55:36 +0930

    Update Vehicle.cpp

--- Vehicle.cpp
+++ Vehicle.cpp
@@ -2,17 +2,10 @@
 #include <ctime>
 #include <iostream>
 using namespace std;
-class Vehicle {
-private:
-  time_t timeOfEntry;
-  int ID;
-
-public:
-  Vehicle() : timeOfEntry(time(0)), ID(0){};
-  Vehicle(int ID) : timeOfEntry(time(0)), ID(ID) {}
-  Vehicle(time_t t, int ID) : timeOfEntry(t), ID(ID) {}
-  int getID() { return ID; }
-  time_t gettimeOfEntry() { return timeOfEntry; }
-  virtual int getParkingDuration() { return 0; }
-};
+Vehicle::Vehicle() : timeOfEntry(time(0)), ID(0){};
+Vehicle::Vehicle(int ID) : timeOfEntry(time(0)), ID(ID) {}
+Vehicle::Vehicle(time_t t, int ID) : timeOfEntry(t), ID(ID) {}
+Vehicle::int getID() { return ID; }
+Vehicle::time_t gettimeOfEntry() { return timeOfEntry; }
+Vehicle::virtual int getParkingDuration() { return 0; }
 
