782abee9df08de489cae48c4c104e4854ac524a4
MikeHE-CODER <a1918791@adelaide.edu.au>
2024-08-27 11:16:22 +0930

    Create Bus.h

---
+++ Bus.h
@@ -0,0 +1,16 @@
+#ifndef Bus_h
+#define Bus_h
+#include "Vehicle.h"
+#include <ctime>
+#include <iostream>
+using namespace std;
+class Bus : public Vehicle {
+public:
+  Bus(){};
+  Bus(int ID) : Vehicle(ID) {}
+  int getParkingDuration() override {
+    return 0.75 * (time(0) - this->gettimeOfEntry());
+  }
+};
+
+#endif
