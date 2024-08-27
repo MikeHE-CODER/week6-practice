62903a5583aebcfd690dac815f43864de69fd924
MikeHE-CODER <a1918791@adelaide.edu.au>
2024-08-27 14:05:40 +0930

    Update ParkingLot.h

--- ParkingLot.h
+++ ParkingLot.h
@@ -41,7 +41,7 @@ public:
       cout << "Vehicle not in the lot" << endl;
     }
   }
-  int coutOverstayingVehicles(int time) {
+  int countOverstayingVehicles(int time) {
     int count = 0;
     for (int i = 0; i < number; i++) {
       if (vehicles[i]->getParkingDuration() > time) {
