a30bb15c048a110991a65c63111883c1b70c2921
MikeHE-CODER <a1918791@adelaide.edu.au>
2024-08-27 11:50:29 +0930

    Update Vehicle.h

--- Vehicle.h
+++ Vehicle.h
@@ -11,6 +11,7 @@ private:
 public:
   Vehicle() : timeOfEntry(time(0)), ID(0){};
   Vehicle(int ID) : timeOfEntry(time(0)), ID(ID) {}
+  Vehicle(time_t t, int ID) : timeOfEntry(t), ID(ID) {}
   int getID() { return ID; }
   time_t gettimeOfEntry() { return timeOfEntry; }
   virtual int getParkingDuration() { return 0; }
