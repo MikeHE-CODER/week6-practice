5b71e07e0644bee9f071096b6d7309038453e4db
MikeHE-CODER <a1918791@adelaide.edu.au>
2024-08-27 12:52:13 +0930

    Update Vehicle.cpp

--- Vehicle.cpp
+++ Vehicle.cpp
@@ -5,7 +5,7 @@ using namespace std;
 Vehicle::Vehicle() : timeOfEntry(time(0)), ID(0){};
 Vehicle::Vehicle(int ID) : timeOfEntry(time(0)), ID(ID) {}
 Vehicle::Vehicle(time_t t, int ID) : timeOfEntry(t), ID(ID) {}
-Vehicle::int getID() { return ID; }
-Vehicle::time_t gettimeOfEntry() { return timeOfEntry; }
-Vehicle::virtual int getParkingDuration() { return 0; }
+int Vehicle::getID() { return ID; }
+time_t Vehicle::gettimeOfEntry() { return timeOfEntry; }
+virtual int Vehicle::getParkingDuration() { return 0; }
 
