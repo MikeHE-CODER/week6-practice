af04046e2dc99e3fff00e93105acd52d2773d368
MikeHE-CODER <a1918791@adelaide.edu.au>
2024-08-27 11:47:15 +0930

    Update Car.cpp

--- Car.cpp
+++ Car.cpp
@@ -6,7 +6,9 @@ class Car : public Vehicle {
 public:
   Car(){};
   Car(int ID) : Vehicle(ID) {}
+  Car(time_t t, int ID) : Vehicle(t, ID) {}
   int getParkingDuration() override {
     return 0.9 * (time(0) - this->gettimeOfEntry());
   }
 };
+
