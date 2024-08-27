52b4652667be4fead5ecbf0f761ca805ca372867
MikeHE-CODER <a1918791@adelaide.edu.au>
2024-08-27 13:23:13 +0930

    Update ParkingLot.h

--- ParkingLot.h
+++ ParkingLot.h
@@ -15,10 +15,10 @@ private:
 public:
   ParkingLot() : max_num(0), vehicles(new Vehicle *[max_num]), number(0){};
   ParkingLot(int max_num)
-      : max_num(max_num), vehicles(new Vehicle *[max_num]){};
+      : max_num(max_num), vehicles(new Vehicle *[max_num]),number(0){};
   int getCount() { return number; }
   void parkVehicle(Vehicle *a) {
-    if (number < max_num - 1) {
+    if (number < max_num) {
       vehicles[number] = a;
       number++;
     } else {
@@ -29,8 +29,12 @@ public:
     int panduan = 0;
     for (int i = 0; i < number; i++) {
       if (vehicles[i]->getID() == ID) {
-        vehicles[i] = nullptr;
+        for (int j=i;j<number-1;j++){
+          vehicles[j]=vehicles[j-1];
+        }
+        number--;
         panduan = 1;
+        break;
       }
     }
     if (panduan == 0) {
