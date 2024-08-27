a3373ce24106d80002499e09202d119d0e909734
MikeHE-CODER <a1918791@adelaide.edu.au>
2024-08-27 12:45:50 +0930

    Update Car.cpp

--- Car.cpp
+++ Car.cpp
@@ -2,12 +2,10 @@
 #include <ctime>
 #include <iostream>
 using namespace std;
-class Car : public Vehicle {
-public:
-  Car(){};
-  Car(int ID) : Vehicle(ID) {}
-  Car(time_t t, int ID) : Vehicle(t, ID) {}
-  int getParkingDuration() override {
+Car::Car(){};
+Car::Car(int ID) : Vehicle(ID) {}
+Car::Car(time_t t, int ID) : Vehicle(t, ID) {}
+Car::int getParkingDuration() override {
     return 0.9 * (time(0) - this->gettimeOfEntry());
   }
 };
