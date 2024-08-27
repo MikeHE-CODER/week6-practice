4461e87c930c3433fe0d33d5f1e12078d31fbe30
MikeHE-CODER <a1918791@adelaide.edu.au>
2024-08-27 12:48:16 +0930

    Update Motorbike.cpp

--- Motorbike.cpp
+++ Motorbike.cpp
@@ -2,13 +2,10 @@
 #include <ctime>
 #include <iostream>
 using namespace std;
-class Motorbike : public Vehicle {
-public:
-  Motorbike(){};
-  Motorbike(int ID) : Vehicle(ID) {}
-  Motorbike(time_t t, int ID) : Vehicle(t, ID) {}
-  int getParkingDuration() override {
+Motorbike::Motorbike(){};
+Motorbike::Motorbike(int ID) : Vehicle(ID) {}
+Motorbike::Motorbike(time_t t, int ID) : Vehicle(t, ID) {}
+Motorbike::int getParkingDuration() override {
     return 0.85 * (time(0) - this->gettimeOfEntry());
   }
-};
 
